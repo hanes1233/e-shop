796264f7a680d8502aa4a570c4dc929a84e68a83
hanes1233 <pa7sha@seznam.cz>
2025-03-22 00:10:45 +0100

    Footer.jsx implementation

--- kidsland-fe/src/components/navigation/Footer.jsx
+++ kidsland-fe/src/components/navigation/Footer.jsx
@@ -1,30 +1,30 @@
 import React from "react";
 import '../../css/Footer.css';
 import { Col, Container, Row } from "react-bootstrap";
-import { facebookImg, instagramImg, twitterImg, youtubeImg } from "../../constants/images";
+import { facebookImg, instagramImg, kidslandLogo, locationImg, mailImg, twitterImg, youtubeImg } from "../../constants/images";
 
 function Footer() {
 
     const handleClick = (e) => {
         const elementId = e.currentTarget.id;
         switch (elementId) {
-            case 'youtube' : {
+            case 'youtube': {
                 window.location.href = 'https://www.youtube.com';
                 break;
             }
-            case 'twitter' : {
+            case 'twitter': {
                 window.location.href = 'https://www.x.com';
                 break;
             }
-            case 'instagram' : {
+            case 'instagram': {
                 window.location.href = 'https://www.instagram.com';
                 break;
             }
-            case 'facebook' : {
+            case 'facebook': {
                 window.location.href = 'https://www.facebook.com';
                 break;
             }
-            default: return(<h4>Unknown operation</h4>);
+            default: return (<h4>Unknown operation</h4>);
         }
     };
 
@@ -33,22 +33,28 @@ function Footer() {
             <Container className="footer">
                 <Row className="footer-header">
                     <Col>
-                        <span className="footer-items">Contact</span>
+                        <h4 className="footer-headers">Contact</h4>
                     </Col>
                     <Col>
-                        <span className="footer-items">Information</span>
+                        <h4 className="footer-headers">Information</h4>
                     </Col>
                     <Col>
-                        <span className="footer-items">Partners</span>
+                        <h4 className="footer-headers">Partners</h4>
                     </Col>
                     <Col className="mb-4">
-                        <span className="footer-items">Follow us</span>
+                        <h4 className="footer-headers">Follow us</h4>
                     </Col>
                 </Row>
                 <Row className="mb-4">
-                    <Col><span className="footer-items">one</span></Col>
-                    <Col><span className="footer-items">one</span></Col>
-                    <Col><span className="footer-items">one</span></Col>
+                    <Col>
+                        <span className="footer-items"><i>Write us</i> {mailImg}</span>
+                    </Col>
+                    <Col>
+                        <span className="footer-items"><i>About</i> <b className="ms-1 kidsland">Kidsland ©</b>{kidslandLogo}</span>
+                    </Col>
+                    <Col>
+                        <span className="footer-items"><i>Cooperation</i></span>
+                    </Col>
                     <Col>
                         <span id="facebook" className="me-4" onClick={handleClick}>
                             {facebookImg}
@@ -59,9 +65,15 @@ function Footer() {
                     </Col>
                 </Row>
                 <Row className="mb-4">
-                    <Col><span className="footer-items">two</span></Col>
-                    <Col><span className="footer-items">two</span></Col>
-                    <Col><span className="footer-items">two</span></Col>
+                    <Col>
+                        <span className="footer-items"><i>Find us</i> {locationImg}</span>
+                    </Col>
+                    <Col>
+                        <span className="footer-items"><i>Compliance</i></span>
+                    </Col>
+                    <Col>
+                        <span className="footer-items"><i>Brands ®</i></span>
+                    </Col>
                     <Col>
                         <span id="twitter" className="me-4" onClick={handleClick}>
                             {twitterImg}
@@ -72,9 +84,15 @@ function Footer() {
                     </Col>
                 </Row>
                 <Row className="mb-4">
-                    <Col className="col-3"><span className="footer-items">three</span></Col>
-                    <Col className="col-3"><span className="footer-items">three</span></Col>
-                    <Col><span className="footer-items">three</span></Col>
+                    <Col className="col-3">
+                        <span className="footer-items"><i>Feedback</i></span>
+                    </Col>
+                    <Col className="col-3">
+                        <span className="footer-items"><i>Career</i></span>
+                    </Col>
+                    <Col>
+                        <span className="footer-items"><i>For media</i></span>
+                    </Col>
                 </Row>
             </Container>
         </>
--- kidsland-fe/src/constants/images.js
+++ kidsland-fe/src/constants/images.js
@@ -4,7 +4,14 @@ export const girlImg = <img className="ms-2" src="/icons/girl.png" width={23} he
 export const tagImg = <img className="ms-2" src="/icons/tag.png" width={23} height={23} alt="tag" />
 
 // Social media
-export const facebookImg = <img className="footer-social-media" src="/icons/facebook.png" atl="facebook-logo" width={50} height={50} />;
-export const instagramImg = <img className="footer-social-media" src="/icons/instagram.png" alt="instagram-logo" width={50} height={50} />;
-export const youtubeImg = <img className="footer-social-media" src="/icons/youtube.png" width={50} height={50} />;
-export const twitterImg = <img className="footer-social-media" src="/icons/twitter.png" width={50} height={50} />;
+export const facebookImg = <img className="footer-social-media" src="/icons/facebook.png" atl="facebook-logo" width={30} height={30} />;
+export const instagramImg = <img className="footer-social-media" src="/icons/instagram.png" alt="instagram-logo" width={30} height={30} />;
+export const youtubeImg = <img className="footer-social-media" src="/icons/youtube.png" width={30} height={30} />;
+export const twitterImg = <img className="footer-social-media" src="/icons/twitter.png" width={30} height={30} />;
+
+// Contacts
+export const mailImg = <img className="ms-2" src="/icons/mail.png" alt="mail-logo" width={25} height={25} />
+export const locationImg = <img className="ms-3" src="/icons/location.png" alt="location-logo" width={25} height={25} />
+
+// Kidsland logo©
+export const kidslandLogo = <img className="ms-2" src="/icon.png" alt="kidsland-logo" width={25} height={25} />
