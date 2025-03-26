089b8ed7165d5525688237b5c89d7e420a7bdf18
hanes1233 <pa7sha@seznam.cz>
2025-03-26 23:16:52 +0100

    Added data to insert-default-data script

--- kidsland-be/liquibase/classpath/0.0.2-insert-default-data.sql
+++ kidsland-be/liquibase/classpath/0.0.2-insert-default-data.sql
@@ -24,4 +24,33 @@ INSERT INTO fc.subcategory (category_id, name)
 VALUES
     ((SELECT id FROM fc.category WHERE name = 'Dress'), 'Boys'),
     ((SELECT id FROM fc.category WHERE name = 'Dress'), 'Girls'),
-    ((SELECT id FROM fc.category WHERE name = 'Dress'), 'Special offer')
+    ((SELECT id FROM fc.category WHERE name = 'Dress'), 'Special offer');
+
+INSERT INTO fc.subcategory (category_id, name)
+VALUES
+    ((SELECT id FROM fc.category WHERE name = 'Shoes'), 'Boys'),
+    ((SELECT id FROM fc.category WHERE name = 'Shoes'), 'Girls'),
+    ((SELECT id FROM fc.category WHERE name = 'Shoes'), 'Special offer');
+
+INSERT INTO fc.subcategory (category_id, name)
+VALUES
+    ((SELECT id FROM fc.category WHERE name = 'Accessories'), 'School'),
+    ((SELECT id FROM fc.category WHERE name = 'Accessories'), 'Party'),
+    ((SELECT id FROM fc.category WHERE name = 'Accessories'), 'Creative'),
+    ((SELECT id FROM fc.category WHERE name = 'Accessories'), 'Tech');
+
+INSERT INTO fc.subcategory (category_id, name)
+VALUES
+    ((SELECT id FROM fc.category WHERE name = 'Furniture'), 'Playroom'),
+    ((SELECT id FROM fc.category WHERE name = 'Furniture'), 'Bedroom'),
+    ((SELECT id FROM fc.category WHERE name = 'Furniture'), 'Outdoor'),
+    ((SELECT id FROM fc.category WHERE name = 'Furniture'), 'Wardrobes'),
+    ((SELECT id FROM fc.category WHERE name = 'Furniture'), 'Special offer');
+
+INSERT INTO fc.subcategory (category_id, name)
+VALUES
+    ((SELECT id FROM fc.category WHERE name = 'For babies'), 'Strollers'),
+    ((SELECT id FROM fc.category WHERE name = 'For babies'), 'Clothing & Apparel'),
+    ((SELECT id FROM fc.category WHERE name = 'For babies'), 'Toys & Games'),
+    ((SELECT id FROM fc.category WHERE name = 'For babies'), 'Feeding'),
+    ((SELECT id FROM fc.category WHERE name = 'For babies'), 'Bath & Skincare');
