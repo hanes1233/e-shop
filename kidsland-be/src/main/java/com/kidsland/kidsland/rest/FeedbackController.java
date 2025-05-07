a6aa0a55da83e2e54ec0e6136208706796da2a14
hanes1233 <pa7sha@seznam.cz>
2025-05-07 21:38:50 +0200

    Test CI/CD BE

--- kidsland-be/src/main/java/com/kidsland/kidsland/rest/FeedbackController.java
+++ kidsland-be/src/main/java/com/kidsland/kidsland/rest/FeedbackController.java
@@ -22,5 +22,5 @@ public class FeedbackController {
         feedbackService.saveFeedback(feedbackDTO);
         return ResponseEntity.ok().build();
     }
-    // Test CI/CD pipeline trigger
+    // Test CI/CD pipeline be trigger
 }
